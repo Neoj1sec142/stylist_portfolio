6468ca256aec01386355cdeb431fc0f03821af4b
Mark Harmon <neothej1sec@gmail.com>
2022-11-08 12:35:38 -0600

    heroku push

--- client/src/store/services/api.js
+++ client/src/store/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const Client = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://meredith-harmon-portfolio.herokuapp.com/',
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
